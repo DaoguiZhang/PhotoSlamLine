bf1121c9d61b9b801f480f38f81d7001aa5f8dfe
DaoguiZhang <m155551651813@163.com>
2025-10-24 15:30:04 +0800

    add line code(2025-10-24)

---
+++ ORB-SLAM3/include/CommonTypeORB.h
@@ -0,0 +1,17 @@
+#ifndef COMMONTYPEORB_H
+#define COMMONTYPEORB_H
+
+#include <vector>
+
+namespace ORB_SLAM3
+{
+
+    #define EPS	(1e-10)
+    #define PI 3.141592653589793
+    //#define OPT_USE_MAHDIST 0
+
+    void PrintErrorInfo(int nErr);
+}
+
+
+#endif  // COMMONTYPEORB_H
